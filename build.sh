96654146d480ec158d56b679f6531f3b3d686840
Tim Veluwenkamp <timveluwenkamp@noxqs.nl>
2014-06-18 10:25:34 +0200

    rebuilded script added PA option

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 # General Settings
 currentdir=$(pwd) 		# Get Current Working Dir 
 now=$(date +'%Y%m%d') # Get Date
-version43=$(echo "4.3") # Android 4.3
-version44=$(echo "4.4") # Android 4.4
+cmversion=$(echo "cm-11") # CM11
+paversion=$(echo "pa-4.4") # CM11 
 getapex='curl -s -o ApexLauncher.apk apex.anddoes.com/Download.aspx' # Download Latest ApexLauncher
 getromstats='curl -s -o RomStats.apk build.codenamelungo.net/RomStats.apk' # Download Latest RomStats
 getmedia='curl -s -o media.zip build.codenamelungo.net/media.zip' # Download Latest Media
 
-# Build android 4.3
-function build_android43 (){
+# Build Cyanogenmod 11
+function build_CM11 (){
 	echo ""
 	echo "What device are you building?"
 	echo -n "enter device name: "
@@ -19,7 +19,7 @@ function build_android43 (){
 	echo "Build Information"
 	echo "-----------------------------"
 	echo "Device:	$DEVICE"
-	echo "Android: $version43"
+	echo "Android: $cmversion"
 	echo "Build Date: $now"
 	echo "-----------------------------"
 	echo ""
@@ -30,87 +30,9 @@ function build_android43 (){
 	sleep 2
 	cd $currentdir/system/app
 	echo "Cleaning Apps..."
-	rm -f Apollo.apk ApplicationsProvider.apk BackupRestoreConfirmation.apk BasicDreams.apk Browser.apk Calculator.apk CMAccount.apk CMFileManager.apk CMUpdater.apk CMWallpapers.apk DocumentsUI.apk DownloadProviderUi.apk DSPManager.apk Email.apk	Email2.apk Exchange2.apk FmRadio.apk Galaxy4.apk HoloSpiralWallpaper.apk HTMLViewer.apk InputDevices.apk LiveWallpapers.apk LiveWallpapersPicker.apk MagicSmokeWallpapers.apk MonthCalendarWidget.apk NoiseField.apk OmniTorch.apk OpenWnn.apk PacProcessor.apk PhaseBeam.apk PhotoTable.apk PicoTts.apk QuickSearchBox.apk SharedStorageBackup.apk Shell.apk SoundRecorder.apk Stk.apk Term.apk Torch.apk Trebuchet.apk VideoEditor.apk VisualizationWallpapers.apk VoiceDialer.apk VoicePlus.apk webaudiores.apk
-	echo "App Cleaning DONE!"
-	sleep 2
-	echo ""
-	echo "Cleaning Libs..."
-	cd $currentdir/system/lib
-	rm -f libjackpal-androidterm4.so libttspico.so *videoeditor*
-	echo "Lib Cleaning DONE!"
-	echo ""
-	echo "Cleaning Media..."
-	rm -rf $currentdir/system/tts
-	rm -rf $currentdir/system/media/video
-	rm -rf $currentdir/system/media/audio/*
-	rm -f $currentdir/system/media/bootanimation.zip
-	echo "Media Cleaning DONE!"
-	sleep 2
-	echo ""
-	echo "Downloading Latest ApexLauncher..."
-	$getapex
-	echo "Adding ApexLauncher..."
-  mv ApexLauncher.apk $currentdir/system/app/
-	echo "ApexLauncher Added!"
-	sleep 2
-	echo ""
-	echo "Downloading Latest ROM Stats..."
-	$getromstats
-	echo "Adding RomStats..."
-  mv Romstats.apk $currentdir/system/app/
-	echo "RomStats Added!"
-	sleep 2
-  echo ""
-  echo "Setting chmod rights"
-  cd $currentdir/system/app
-  chmod +x ApexLauncher.apk
-  chmod +x RomStats.apk
-  sleep 2
-	echo ""
-	echo "Downloading Latest Codename Lungo Media..."
-	cd $currentdir/system/media/audio
-	$getmedia
-	echo "Adding Codename Lungo Media..."
-	unzip -q media.zip
-	rm -f media.zip
-	echo "Codename Lungo Media Added!"
-	sleep 2
-	echo ""
-	echo "Opening build.prop for Cleaning..."
-	mate $currentdir/system/build.prop
-	read -p "Build.prop is open press [enter] when you are done"
-	echo ""
-	cd $currentdir
-	find . -name '*.DS_Store' -type f -delete
-	7z a CodenameLungo-$DEVICE$version43-$now.zip
-	mv CodenameLungo-$DEVICE$version43-$now.zip ~/Downloads
-	echo "Build is done!"
-}
-
-# Build android 4.4
-function build_android44 (){
-	echo ""
-	echo "What device are you building?"
-	echo -n "enter device name: "
-	read -e DEVICE
-	echo ""
-	echo "Build Information"
-	echo "-----------------------------"
-	echo "Device:	$DEVICE"
-	echo "Android: $version44"
-	echo "Build Date: $now"
-	echo "-----------------------------"
-	echo ""
-	read -p "Everything ok? press [enter] to continue"
-	echo ""
-	echo "Starting Build!"
-	echo ""
-	sleep 2
-	cd $currentdir/system/app
-	echo "Cleaning Apps..."
-	rm -fv Apollo.apk ApplicationsProvider.apk BasicDreams.apk Browser.apk Calculator.apk CMAccount.apk CMFileManager.apk CMUpdater.apk CMWallpapers.apk DocumentsUI.apk DownloadProviderUi.apk DSPManager.apk Email.apk	Email2.apk Exchange2.apk FM2.apk  FmRadio.apk FMRecord.apk Galaxy4.apk HoloSpiralWallpaper.apk HTMLViewer.apk InputDevices.apk LiveWallpapers.apk LiveWallpapersPicker.apk LockClock.apk MagicSmokeWallpapers.apk MonthCalendarWidget.apk NoiseField.apk OmniTorch.apk OpenWnn.apk PacProcessor.apk PhaseBeam.apk PhotoTable.apk PicoTts.apk QuickSearchBox.apk SharedStorageBackup.apk Shell.apk SoundRecorder.apk Stk.apk Term.apk Torch.apk Trebuchet.apk VideoEditor.apk VisualizationWallpapers.apk VoiceDialer.apk VoicePlus.apk webaudiores.apk WhisperPush.apk
+	rm -fv Apollo.apk BasicDreams.apk Browser.apk Calculator.apk CMFileManager.apk CMWallpapers.apk DocumentsUI.apk DownloadProviderUi.apk DSPManager.apk Email.apk Exchange2.apk Galaxy4.apk HoloSpiralWallpaper.apk HTMLViewer.apk LiveWallpapers.apk LiveWallpapersPicker.apk LockClock.apk MagicSmokeWallpapers.apk NoiseField.apk PacProcessor.apk PhaseBeam.apk PhotoTable.apk SoundRecorder.apk Stk.apk Term.apk Torch.apk VideoEditor.apk VisualizationWallpapers.apk
 	cd $currentdir/system/priv-app
-	rm -fv BackupRestoreConfirmation.apk InputDevices.apk Launcher3.apk OpenDelta.apk VoiceDialer.apk CMS.apk CMUpdater.apk SharedStorageBackup.apk Trebuchet.apk PicoTts.apk
+	rm -fv BackupRestoreConfirmation.apk CMAccount.apk CMUpdater.apk InputDevices.apk PicoTts.apk Trebuchet.apk VoiceDialer.apk
 	echo "App Cleaning DONE!"
 	sleep 2
 	echo ""
@@ -163,17 +85,101 @@ function build_android44 (){
 	read -p "Build.prop is open press [enter] when you are done"
 	echo ""
 	cd $currentdir
+	find . -name '.DS_Store' -type f -delete
 	find . -name '*.DS_Store' -type f -delete
-	7z a CodenameLungo-$DEVICE$version44-$now.zip
-	mv CodenameLungo-$DEVICE$version44-$now.zip ~/Downloads
+	7z a CodenameLungo-$cmversion-$now-$DEVICE.zip
+	mv CodenameLungo-$cmversion-$now-$DEVICE.zip ~/Downloads
 	echo "Build is done!"
 }
+
+# Build Paranoid Android
+function build_pa (){
+	echo ""
+	echo "What device are you building?"
+	echo -n "enter device name: "
+	read -e DEVICE
+	echo ""
+	echo "Build Information"
+	echo "-----------------------------"
+	echo "Device:	$DEVICE"
+	echo "Android: $paversion"
+	echo "Build Date: $now"
+	echo "-----------------------------"
+	echo ""
+	read -p "Everything ok? press [enter] to continue"
+	echo ""
+	echo "Starting Build!"
+	echo ""
+	sleep 2
+	cd $currentdir/system/app
+	echo "Cleaning Apps..."
+	rm -fv BasicDreams.apk Browser.apk Calculator.apk DocumentsUI.apk DownloadProviderUi.apk Email.apk Exchange2.apk Galaxy4.apk HoloSpiralWallpaper.apk HTMLViewer.apk Launcher3.apk Lightbulb.apk LiveWallpapers.apk LiveWallpapersPicker.apk MagicSmokeWallpapers.apk NoiseField.apk OmniTorch.apk OpenWnn.apk PacProcessor.apk PhaseBeam.apk PhotoTable.apk SoundRecorder.apk Stk.apk VideoEditor.apk VisualizationWallpapers.apk
+	cd $currentdir/system/priv-app
+	rm -fv InputDevices.apk MusicFX.apk ParanoidOTA.apk
+	echo "App Cleaning DONE!"
+	sleep 2
+	echo ""
+	echo "Cleaning Libs..."
+	cd $currentdir/system/lib
+	rm -f *videoeditor*
+	echo "Lib Cleaning DONE!"
+	echo ""
+	echo "Cleaning Media..."
+	rm -rf $currentdir/system/tts
+	rm -rf $currentdir/system/media/video
+	rm -rf $currentdir/system/media/audio/*
+	rm -f $currentdir/system/media/bootanimation.zip
+	echo "Media Cleaning DONE!"
+	sleep 2
+	echo ""
+	echo "Downloading Latest ApexLauncher..."
+	$getapex
+	mv ApexLauncher.apk $currentdir/system/app/
+	echo "Adding ApexLauncher..."
+	echo "ApexLauncher Added!"
+	sleep 2
+	echo ""
+	echo "Downloading Latest RomStats..."
+	cd $currentdir/system/app/
+	$getromstats
+	echo "Adding RomStats..."
+	echo "ROMStats Added!"
+	sleep 2
+  echo ""
+  echo "Setting chmod rights"
+  cd $currentdir/system/app 
+  chmod +x ApexLauncher.apk
+  chmod +x RomStats.apk
+  echo "Setting chmod rights Done!"
+  sleep 2
+	echo ""
+	echo "Downloading Latest Codename Lungo Media..."
+	cd $currentdir/system/media/audio
+	$getmedia
+	echo "Adding Codename Lungo Media..."
+	unzip -q media.zip
+	rm -f media.zip
+	echo "Codename Lungo Media Added!"
+	sleep 2
+	echo ""
+	echo "Opening build.prop for Cleaning..."
+	mate $currentdir/system/build.prop
+	read -p "Build.prop is open press [enter] when you are done"
+	echo ""
+	cd $currentdir
+	find . -name '.DS_Store' -type f -delete
+	find . -name '*.DS_Store' -type f -delete
+	7z a CodenameLungo-$paversion-$now-$DEVICE.zip
+	mv CodenameLungo-$paversion-$now-$DEVICE.zip ~/Downloads
+	echo "Build is done!"
+}
+
 
 show_header (){
 	echo ""
 	echo "Codename Lungo Build Script "
 	echo "---------------------------------------------------------"
-	echo "Version     	: 20140616"
+	echo "Version     	: 20140618"
 	echo "Maintainer  	: T.Veluwenkamp <contact@timveluwenkamp.eu>"
 	echo "Copyright   	: Copyright (C) 2014 T.Veluwenkamp"
 	echo "---------------------------------------------------------"
@@ -183,8 +189,8 @@ show_header (){
 show_menu() {
 	echo "Select Android Version"
 	echo "----------------------"
-	echo "1. Android 4.4"
-	echo "2. Android 4.3"
+	echo "1. Cyanogenmod 11"
+	echo "2. Paranoid Android"
 	echo "3. Exit"
 }
 
@@ -192,8 +198,8 @@ function read_input(){
 	local c
 	read -p "Enter build target [ 1 - 3 ] " c
 	case $c in
-		1)	build_android44 ;;
-		2)	build_android43 ;;
+		1)	build_CM11 ;;
+		2)	build_pa ;;
 		3)	echo "Good Bye!"; exit 0; ;;
 		*)	
 			echo "Please select between 1 to 3 choice only."
